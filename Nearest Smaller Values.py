6bf1f5d5b309cec24728e308c56bb69dd8e4543d
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2022-01-15 23:30:03 +0530

    Update Nearest Smaller Values.py

--- Nearest Smaller Values.py
+++ Nearest Smaller Values.py
@@ -1,8 +1,8 @@
-'''Have a function NearestSmallerValues(arr) take the array of integers stored in arr, and for each element in the list, search all the previous values for the nearest element
-  that is smaller than (or equal to) the current element and create a new list of this numbers'''
+'''Have a function NearestSmallerValues(arr) take the array of integers stored in arr, and for each element in the list, 
+  search all the previous values for the nearest element that is smaller than (or equal to) the current element and create a new list of this numbers.
+  If there is no element before a certain position that is samller, input a -1.'''
 
-'''If there is no element before a certain position that is samller, input a -1.
-  For example, arr is [5, 2, 8, 3, 9, 12],
+'''For example, arr is [5, 2, 8, 3, 9, 12],
   Than nearest samller values list is [-1, -1, 2, 2, 3, 9]
 '''
 
